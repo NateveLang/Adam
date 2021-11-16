06fa6ab46fc84e056b5549d691783bb223578219
eanorambuena <eanorambuena@uc.cl>
2021-11-16 18:26:11 -0300

    Transform tuples to lists

--- driver_pytest.py
+++ driver_pytest.py
@@ -1,10 +1,10 @@
 import subprocess
 
 def build(*args):
-    subprocess.call(["py", "-m", "nqa", "build"] + args)
+    subprocess.call(["py", "-m", "nqa", "build"] + list(args))
 
 def compile(*args):
-    subprocess.call(["py", "-m", "nqa", "compile"] + args)
+    subprocess.call(["py", "-m", "nqa", "compile"] + list(args))
 
 def run(*args):
-    subprocess.call(["py", "-m", "nqa", "run"] + args)
+    subprocess.call(["py", "-m", "nqa", "run"] + list(args))
