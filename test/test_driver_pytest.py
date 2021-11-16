c2a93f7a0b4a90472ecd2ad185fa32e68d7266c5
eanorambuena <eanorambuena@uc.cl>
2021-11-16 18:41:14 -0300

    Add content tests

--- test/test_driver_pytest.py
+++ test/test_driver_pytest.py
@@ -5,6 +5,11 @@ n = 2
 def test_build():
     for i in range(1, n + 1):
         dp.build("test/example" + str(i) + ".nateve")
+        f1 = open("test/example" + str(i) + ".py", "r")
+        f2 = open("test/example" + str(i) + ".txt", "r")
+        assert f1.read() == f2.read()
+        f1.close()
+        f2.close()
 
 def test_compile():
     for i in range(1, n + 1):
